1b42d439e4ca0b8fc9b9092ee9492899d81868e7
Ritovan03 <ritovan0302@gmail.com>
2025-10-30 19:36:33 +0000

    fix the code and removed unnc=eccesary thing. optimised the makefile by removing unncessary packages

--- tools/benchmarks/codimensional.py
+++
@@ -1,7 +0,0 @@
-from print_fixtures import print_fixtures
-
-print_fixtures("codimensional", [
-    "codimensional/edges/chain-net-16x16-ball",
-    # "codimensional/edges/sphere-needle-bed",
-    # "codimensional/points/sphere-pc-sphere-planes"
-])
